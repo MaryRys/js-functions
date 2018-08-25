0f7cf748e89ec6915d695a1aa35910872c99a318
Mary Rys <rys.mary@gmail.com>
2018-08-25 11:49:13 -0500

    made write to DOM

--- functions.js
+++ functions.js
@@ -20,3 +20,19 @@ const nomnom = (person, food) => {
 const bearSticks = nuggetizer('bear');
 console.log(nomnom('mary', bearSticks));
 
+// function numberAdder that takes in a number and returns the number plus 3. so input =22 output =25
+
+const numberAdder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+};
+console.log('num: ', numberAdder(22)); 
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+numberAdder(22);
+numberAdder(2);
+numberAdder(4);
+numberAdder(22);
