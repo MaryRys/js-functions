9d4e578d2fe9a96c7734e52f56b0eae9b7fcfb2b
Mary Rys <rys.mary@gmail.com>
2018-08-25 10:27:02 -0500

    object shorthand notation

--- main.js
+++ main.js
@@ -19,3 +19,16 @@ greetingElement.innerHTML = document.getElementById ('greeting');
 // greetingElement.innerHTML = 'Hello my name is ' + firstName + lastName;
 
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+// object shorthand notation
+let name = 'callan';
+let age = '100000';
+
+// let person = {
+//     name: name,
+//     age: age
+// };
+
+let person = {name, age};
+
+console.log('person:', person.name);
